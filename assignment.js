8baae8230ed99b34488e60b63683c848e0139a24
Suresh Chakma <34184573+sureshchakma@users.noreply.github.com>
2020-07-17 19:05:35 +0600

    feetToMile

---
+++ assignment.js
@@ -0,0 +1,10 @@
+//Fisrt question to answer....
+
+function feetToMile(feet){
+    var mile= feet / 5280  // 1 mile=5280 feets;
+    return mile;
+}
+
+var result = feetToMile(20000);
+
+console.log(result);
