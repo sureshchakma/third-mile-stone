916fa74520e649f598b76a9adf5c67074d4d517d
Suresh Chakma <34184573+sureshchakma@users.noreply.github.com>
2020-07-17 21:55:17 +0600

    Javascript problem solving

--- assignment.js
+++ assignment.js
@@ -24,9 +24,27 @@ var totalQubitWood= woodCalculator(5, 6, 4);
 
 console.log(totalQubitWood);
 
-//#third question to answer...#brickCalculator
+//#third question to answer...#brickCalculator;
+function brickCalculator(floor){
+    
+    if(floor <=10){
+        var feet1= floor * 15;
+        var bricks=feet1 * 1000;
+    }
 
+    else if(floor <=20){
+        var feet2 =floor * 12;
+        var bricks =feet2 * 1000;
+    }
+    else if(floor >=20){
+        feet3 = floor * 10;
+        var bricks =feet3 * 1000;
+    }
+    return bricks;
+}
+var output =brickCalculator(10);
 
+console.log(output);
 
 
 
